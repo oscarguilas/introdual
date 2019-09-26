261d6ff5a70de72ee92b3dc13f036d2791cd1cd4
oscarguilas <cf19oscar.guillen@iesjoandaustria.org>
2019-09-26 17:31:50 +0200

    Segundo commit

--- app/src/main/java/com/example/introdual/MainActivity.java
+++ app/src/main/java/com/example/introdual/MainActivity.java
@@ -2,15 +2,20 @@ package com.example.introdual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     //Variables
     TextView questionText;
-    Button buttonCorrect, buttonIncorrect;
+    Button buttonTrue, buttonFalse;
+    int[] shownQuestions;
+    boolean correctAnswer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,22 +24,51 @@ public class MainActivity extends AppCompatActivity {
 
         declareVars();
 
+        getNewQuestion(){
 
+        }
     }
 
     public void declareVars(){
         questionText = findViewById(R.id.textQuestion);
-        buttonCorrect = findViewById(R.id.buttonCorrect);
-        buttonIncorrect = findViewById(R.id.buttonIncorrect);
+        buttonTrue = findViewById(R.id.buttonTrue);
+        buttonFalse = findViewById(R.id.buttonFalse);
     }
 
     public void optionSelect(View view){
         int viewId = view.getId();
-        if (viewId == buttonCorrect.getId()) {
+        Intent intent = new Intent(this, ResultsActivity.class);
+        if (viewId == buttonTrue.getId()) {
             //Correcto, pasar a ResultsActivity
-        } else if (viewId == buttonIncorrect.getId()) {
+            intent.putExtra("result", true);
+            startActivity(intent);
+        } else if (viewId == buttonFalse.getId()) {
             //Incorrecto, pasar a ResultsActivity
+            intent.putExtra("result", false);
+            startActivity(intent);
         } else { /*error*/ }
+    }
+
+    public void checkResult(boolean click){
+        //Para expandir mas tarde
+    }
+
+    public void getNewQuestion(){
+        Random rng = new Random();
+        int i = rng.nextInt(10);
+
+    }
+
+    public boolean hasBeenShown(int check){ // Por acabar
+        int i = 0;
+        boolean feedback = false;
+        while(i<shownQuestions.length){
+            if(shownQuestions[i] == check) feedback = true;
+            i++;
+        }
+
+        return feedback;
+
     }
 
 }
--- app/src/main/java/com/example/introdual/ResultsActivity.java
+++ app/src/main/java/com/example/introdual/ResultsActivity.java
@@ -2,13 +2,29 @@ package com.example.introdual;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultsActivity extends AppCompatActivity {
+    TextView resultsText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        resultsText = findViewById(R.id.resultsText);
+        Intent formerIntent = getIntent();
+
+        setResultText(formerIntent);
+    }
+
+    private void setResultText(Intent intent){
+        if(intent.getExtras().getBoolean("result")){
+            resultsText.setText(getString(R.string.resultCorrect));
+        } else if (!intent.getExtras().getBoolean("result")){
+            resultsText.setText(getString(R.string.resultIncorrect));
+        }
     }
 }
